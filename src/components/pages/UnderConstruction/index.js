a7d59447982943ff777d10cfcff78f2b69cc4ce6
Mithun-18 <mithunpoojary180@gmail.com>
2024-10-25 16:11:02 +0530

    final commit

--- src/components/pages/UnderConstruction/index.js
+++ src/components/pages/UnderConstruction/index.js
@@ -1,11 +1,8 @@
-import { faPersonDigging } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './index.css';
 
 const UnderConstruction = () => {
   return (
-    <div className="container under-construction">
-      <FontAwesomeIcon icon={faPersonDigging} size="3x" />
+    <div className="container under-construction" style={{display:'flex',alignItems:"center",justifyContent:"center"}}>
       <h2>Page under construction</h2>
     </div>
   );
